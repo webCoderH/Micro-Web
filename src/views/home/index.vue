f0fd59f4f69126eaf6786cab39a9f14f0887fa05
KidneyFlower <753475777@qq.com>
2017-06-13 11:24:05 +0800

    :sparkles: 项目脚手架优化

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <h1>Hello {{name}}!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        name: 'Yo Bitch'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+  @import './style.scss';
+</style>
