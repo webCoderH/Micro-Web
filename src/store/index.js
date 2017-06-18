982bfccba10d0a2dd45df4cf3cd60cf63f36bb02
KidneyFlower <753475777@qq.com>
2017-06-18 23:38:43 +0800

    :sparkles: 添加了Vuex Demo

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as actions from './actions'
+import * as mutations from './mutations'
+import home from './modules/home'
 
 Vue.use(Vuex)
 
-const state = {}
-
 export default new Vuex.Store({
-  state
+  getters,
+  actions,
+  mutations,
+  modules: {
+    home
+  }
 })
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="home">
     <h1>Hello {{name}}!</h1>
+    <h2>You have fucked bitch for {{count}} times.</h2>
+    <div class="buttons">
+      <button @click="addCount">Fuck Bitch once</button>
+      <button @click="addCountAsync">Fuck Bitch once for 1 second</button>
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapState, mapMutations, mapActions } from 'vuex'
+  import * as types from 'src/store/mutation-types'
+
   export default {
-    data () {
-      return {
-        name: 'Yo Bitch'
-      }
+    computed: {
+      ...mapState('home', [
+        'name',
+        'count'
+      ])
+    },
+    methods: {
+      ...mapActions('home', [
+        types.ADD_COUNT_ASYNC
+      ]),
+      ...mapMutations('home', [
+        types.ADD_COUNT
+      ])
     }
   }
 </script>
